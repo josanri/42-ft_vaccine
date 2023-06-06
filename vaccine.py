a7ba4b34dd208399db75dfcc2eb0aa706cc9cfa3
Jose Manuel Sanchez Rico <josesanc@c2r8s2.42malaga.com>
2023-06-06 20:29:44 +0200

    URL required parameter

--- vaccine.py
+++ vaccine.py
@@ -3,7 +3,7 @@ import argparse
 from src.sql_injection_injector import SQLInjector
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Vaccine ./vaccine.py [-o output_file, -X method]')
+    parser = argparse.ArgumentParser(description='Vaccine ./vaccine.py [-o output_file, -X method] URL')
     parser.add_argument('-o', '--output',
                         action='store',
                         help="specify archive file")
@@ -14,5 +14,8 @@ if __name__ == "__main__":
                         nargs='?',
                         choices=["GET", "POST", "PUT", "DELETE"],
                         help="HTTP method for executing the sql injection")
+    parser.add_argument('URL',
+                        action='store',
+                        help="URL where injections will be done")
     args = parser.parse_args()
     SQLInjector(args.output, args.X)
