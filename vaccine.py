ebec48224b490c54b87380dfecd1465451290890
Jose Manuel Sanchez Rico <josesanc@c2r8s2.42malaga.com>
2023-06-06 17:34:26 +0200

    Argument parser

--- vaccine.py
+++ vaccine.py
@@ -1,2 +1,15 @@
+import argparse
+
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(description='Vaccine ./vaccine.py [-o output_file, -X method]')
+    parser.add_argument('-o', '--output',
+                        action='store',
+                        help="specify archive file")
+    parser.add_argument('-X',
+                        action='store',
+                        default='GET',
+                        const='GET',
+                        nargs='?',
+                        choices=["GET", "POST", "PUT", "DELETE"],
+                        help="HTTP method for executing the sql injection")
+    args = parser.parse_args()
