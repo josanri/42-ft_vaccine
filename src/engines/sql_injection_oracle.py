40162ab220e7a8927e930e0435ef61b8d5ce4af0
Jose Manuel Sanchez Rico <josesanc@c2r10s1.42malaga.com>
2023-06-12 12:31:28 +0200

    Removed oracle engine

--- src/engines/sql_injection_oracle.py
+++
@@ -1,7 +0,0 @@
-from sql_injection_engine import SQLInjectionEngine
-
-class OracleInjection(SQLInjectionEngine):
-    def version():
-        return "SELECT VERSION, VERSION_FULL FROM V$INSTANCE;"
-    
-    # TODO: Bonus for the project
