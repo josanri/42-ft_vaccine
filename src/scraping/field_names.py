0180d804e5b3282e99652c43886bcc6997ce1e07
Jose Manuel Sanchez Rico <josesanc@c2r10s1.42malaga.com>
2023-06-12 11:11:00 +0200

    Moved common inputs to form spider
    Added default values

--- src/scraping/field_names.py
+++
@@ -1,4 +0,0 @@
-USUAL_FIELD_NAMES = (
-    'id', 'identifier', 'user', 'name', 'email', 'phone',
-    'address', 'city', 'state', 'country', 'date', 'time',
-    'updated', 'accessed', 'created', 'event')
--- src/scraping/form_spider.py
+++ src/scraping/form_spider.py
@@ -7,7 +7,6 @@ import requests
 
 from src.scraping.scrape_error import ScrapeError
 
-
 @dataclasses.dataclass
 class InputInfo:
     type: str
@@ -22,6 +21,24 @@ class FormInfo:
     action: str
     inputs: typing.List[InputInfo]
 
+COMMON_GET_INPUTS = (
+    InputInfo('number', 'id', 'id', 1),
+    InputInfo('number', 'identifier', 'identifier', 1),
+    InputInfo('text', 'user', 'user', "example"),
+    InputInfo('number', 'name', 'name', 3),
+    InputInfo('email', 'email', 'email', "valid@gmmail.com"),
+    InputInfo('tel', 'phone', 'phone', "123-456-7890"),
+    InputInfo('text', 'city', 'city', "Malaga"),
+    InputInfo('text', 'state', 'state', "ES"),
+    InputInfo('text', 'country', 'country', "Spain"),
+    InputInfo('date', 'date', 'date', "2022-10-11"),
+    InputInfo('text', 'address', 'address', "Calle Marques de Larios"),
+    InputInfo('number', 'time', 'time', "09:45"),
+    InputInfo('number', 'updated', 'updated', 1),
+    InputInfo('number', 'accessed', 'accessed', 1),
+    InputInfo('number', 'created', 'created', 1),
+    InputInfo('number', 'event', 'event', 1),
+)
 
 class FormSpider:
     def __init__(self, url: str):
