9b3d6e261006976c3318722992acd6987b702b91
Jose Manuel Sanchez Rico <josesanc@c2r10s1.42malaga.com>
2023-06-12 10:23:51 +0200

    Removed quote from OR and return self

--- src/sql_injection_builder.py
+++ src/sql_injection_builder.py
@@ -20,14 +20,23 @@ class SQLInjectionBuilder:
         else:
             return self.final_string
 
+    def add_single_quote(self):
+        self._add("'")
+        return self
+
     def or_boolean_one_equals_one_quoted(self):
-        self._add("' OR 1=\"1\"")
+        self._add(" OR 1=\"1\"")
+        return self
+
 
     def or_boolean_one_equals_one_unquoted(self):
-        self._add("' OR 1=1")
+        self._add(" OR 1=1")
+        return self
 
     def and_false_condition(self) -> None:
         self._add("' AND 1=\"2\"")
+        return self
 
     def union_subquery(self, subquery: str) -> None:
         self._add(f" UNION {subquery}")
+        return self
